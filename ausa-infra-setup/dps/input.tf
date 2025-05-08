aecc098531551cc33dde316d1187b690b14b96ce
Alton1998 <altondsouza02@gmail.com>
2025-05-08 12:13:08 -0600

    Fix

---
+++ ausa-infra-setup/dps/input.tf
@@ -0,0 +1,10 @@
+variable "storage_account_name" {
+  description = "Storage account name for IoT Hub"
+  default = "ausa_iot_hub_storage_account"
+}
+
+variable "resource_group_name" {
+  description = "Resource Group Name"
+  default = "ausa_resource_group"
+}
+
