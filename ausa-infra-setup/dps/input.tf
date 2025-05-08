a03bcf47462622eaeb63f6aea8992e5cbcae0f03
Alton1998 <altondsouza02@gmail.com>
2025-05-08 13:25:37 -0600

    Added storage container

--- ausa-infra-setup/dps/input.tf
+++ ausa-infra-setup/dps/input.tf
@@ -7,4 +7,22 @@ variable "resource_group_name" {
   description = "Resource Group Name"
   default = "ausa_resource_group"
 }
+variable "resource_group_location" {
+  default = "east_us"
+  description = "Resource Group loac"
+}
 
+variable "storage_account_tier" {
+  default = "Standard"
+  description = "IoT Hub Storage Account Tier"
+}
+
+variable "account_replication_type" {
+  default = "LRS"
+  description = "IoT Hub Storage Account Replication Type"
+}
+
+variable "azurerm_storage_container_name" {
+  default = "ausa_iot_hub_storage_container"
+  description = "IoT hub storage container name"
+}
--- ausa-infra-setup/dps/main.tf
+++ ausa-infra-setup/dps/main.tf
@@ -1,7 +1,20 @@
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  location                 = var.resource_group_location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.account_replication_type
+}
+
+resource "azurerm_storage_container" "ausa_iot_hub_storage_container" {
+  name                  = "mycontainer"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+resource "azurerm_eventhub_namespace" "namespace" {
+  name                = random_pet.eventhub_namespace_name.id
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
 }
